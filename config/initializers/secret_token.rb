3d728d0773ad17b9fdc2e2f1512da1972039278c
Ziming Miao <miaoziming@wandoujia.com>
2012-06-27 00:33:11 +0800

    Initialize Rails 3.2.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParacutinMe::Application.config.secret_token = '95375d03bc90c52be722b172c82485c8a0e4b01342d8a1864802904d0e452dc22e63acc88f2fe4065ac1f463aec58fd1feffa34dc16e273cde4198f7e27b105e'
